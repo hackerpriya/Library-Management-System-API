e0bb960490d3b51de9b34282aba356008d4259e1
Priyanka Bhargava <priyanka.bhrgva@gmail.com>
2025-02-09 16:46:29 +0530

    User Model Created

---
+++ models/User.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+    name : String,
+    email : {type:String, unique: true, required:true},
+    password : {
+        type:String, required:true
+    },
+    role : {
+        type: String, enum: ['admin','member'],
+        default: 'member'
+    }
+
+})
+
+
+module.exports = mongoose.model('User',UserSchema)
